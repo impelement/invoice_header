e532943e6b699b694d162fd2edfa224477ae7772
iraj <iraj.amini@outlook.com>
2024-11-25 15:10:23 -0500

    initial commit

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Invoice report",
+    'version': '17.0.1.0.0',
+    "summary": "",
+    "author": "",
+    "summary": " ",
+    "license": "AGPL-3",
+    "maintainer": "SereneFox Solutions",
+    "website": "https://serenefoxsolutions.com",
+    "author": "SereneFox Solutions",
+    "depends": ["account"],
+    "data": [
+        "views/invoice_report_template.xml"
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'license': 'LGPL-3',
+}
